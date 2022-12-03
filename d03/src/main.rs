d8c65089f8f583a0fb5814fa9f24e1d714a3bf6b
relativehysteria <rruziro@gmail.com>
2022-12-03 10:58:23 +0100

    Day 3 Part 2

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,24 +1,54 @@
+#![feature(array_chunks)]
+// `array_chunks()` is used instead of `chunks_exact()` because it works :)
+
 use std::fs::read_to_string;
 
 fn main() {
-    // Read the input
+    // Read the input, separate it by newlines
     let input = read_to_string("input").unwrap();
+    let input = input.lines().map(str::as_bytes);
 
-    // Turn each line into two compartments
-    let bags = input.lines()
-        // Turn the characters into bytes
-        .map(str::as_bytes)
-        .filter_map(|line| {
-            // Split the line into two compartments
-            let (a, b) = line.split_at(line.len()/2);
+    // Letters are turned into their numeral priority form using this formula
+    let prio = |x: &u8| -> usize {
+        (*x as usize - 38) - 58 * ((*x >= b'a') as usize)
+    };
 
-            a.iter()
-                // Find the common letter
-                .find(|&x| b.contains(x))
-                // Turn it into its numeral form
-                .map(|&x| (x as usize - 38) - 58 * ((x >= b'a') as usize))
-        })
-        .sum::<usize>();
+    // Part 1
+    {
+        // Turn each bag into two compartments
+        let priorities = input
+            .clone()
+            .filter_map(|line| {
+                // Split the line into two compartments
+                let (a, b) = line.split_at(line.len()/2);
 
-    println!("{bags}");
+                a.iter()
+                    // Find the common letter
+                    .find(|&x| b.contains(x))
+                    // Turn it into its numeral form
+                    .map(prio)
+            })
+            // Sum it up
+            .sum::<usize>();
+
+        println!("{priorities}");
+    }
+
+    // Part 2
+    {
+        let priorities = input
+            .collect::<Vec<&[u8]>>()
+            // Go through 3 lines at a time
+            .array_chunks::<3>()
+            .filter_map(|[a, b, c]| {
+                a.iter()
+                    // Find the common letter in all of them
+                    .find(|&x| b.contains(x) && c.contains(x))
+                    // Turn it into its numeral form
+                    .map(prio)
+            })
+            // Sum it up
+            .sum::<usize>();
+        println!("{priorities}");
+    }
 }
